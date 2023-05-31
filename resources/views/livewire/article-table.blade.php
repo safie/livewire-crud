7eb8570d836bec3b02b64934ec9ff62342ea6b80
safie <safie19@gmail.com>
2023-05-31 16:09:05 +0800

    first commit

---
+++ resources/views/livewire/article-table.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Article Table Component</h1>
+</div>
---
+++ resources/views/tutorial.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutorial Livewire</title>
+    @livewireStyles
+</head>
+
+<body>
+    <h1>Tutorial Livewire by Safie</h1>
+    @livewire('article-form')
+    @livewire('article-table')
+    @livewireScripts
+</body>
+
+</html>
